b86a9a97d9d046a6d458e5c90a386b370f5439ae
Unknown <renaud.hpavie@gmail.com>
2020-10-20 12:11:26 +0200

    Initialisation / Ajout bootstrap/fullpage...
    Mise en place du projet

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var myFullpage = new fullpage('#fullpage', {
+    anchors: ['Home', 'About', 'Portfolio', 'Skills'],
+    sectionsColor: ['#1e232b', '#1e232b', '#1e232b', '#1e232b'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['First page', 'Second page', 'Third and last page','fourth'],
+    verticalCentered:false,
+});
