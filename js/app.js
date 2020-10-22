8939b0843405f222ae3bf4005b7efd38ffec23b5
Unknown <renaud.hpavie@gmail.com>
2020-10-22 16:11:18 +0200

    Page skills + modifs global

---
+++ js/app.js
@@ -0,0 +1,19 @@
+let observer = new IntersectionObserver(function (observables){
+    observables.forEach(function (observable){
+        if (observable.intersectionRatio > 0.50){
+            observable.target.classList.remove('not-visible')
+            // observer.unobserve(observable.target)
+        } else {
+            observable.target.classList.add('not-visible')
+        }
+    })
+}, {
+    threshold: [0.50]
+
+})
+
+let items = document.querySelectorAll('.text, .image, .animation-top, .animation-top-left, .animation-top-right, .animation-bottom, .animation-bottom-left, .animation-bottom-right, .animation-left, .animation-right')
+items.forEach(function (item){
+    item.classList.add('not-visible')
+    observer.observe(item)
+})
